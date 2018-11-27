2165fb0806405f02dd6455d42c35354d961bf980
Matheus Rodrigues <matheusrod92@gmail.com>
2018-11-26 23:43:38 -0200

    version 2.5 with gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,114 @@
-const gulp = require('gulp'),
-    sass = require ('gulp-sass'),
-    notify = require('gulp-notify'),
-    filter = require('gulp-filter'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin')
-    browserSync = require('browser-sync'),
-    bulkSass = require('gulp-sass-bulk-import'),
-    gutil = require('gulp-util'),
-    babel = require('gulp-babel');
+/* IMPORTS */
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync');
+const bulkSass = require('gulp-sass-bulk-import');
+const concat = require('gulp-concat');
+const filter = require('gulp-filter');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const image = require('gulp-image');
+const nunjucks = require('gulp-nunjucks');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+
+/* CONFIGS */
 
 const config = {
-  stylesPath: 'assets/styles',
-  jsPath: 'assets/scripts',
-  imagesPath: 'assets/images',
+  stylesPath: 'src/assets/styles',
+  jsPath: 'src/assets/scripts',
+  imagesPath: 'src/assets/images',
   outputDir: 'public/dist',
-  htmlPath: 'public/'
+  htmlPath: 'public/',
+  layoutsPath: 'src/layouts',
 };
 
 const tasks = [
-  'icons',
+  'bundle',
   'css',
-  'jquery',
-  'popper',
-  'bootstrap-js',
-  'js',
   'images',
+  'js',
+  'layouts',
 ];
 
-gulp.task('icons', function() { 
-  return gulp.src('./node_modules/font-awesome/fonts/**.*') 
-    .pipe(gulp.dest(config.outputDir + '/fonts')); 
+/* ASSETS */
+
+gulp.task('bundle', function () {
+  return gulp.src([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/popper.js/dist/umd/popper.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+  ])
+  .pipe(concat('bundle.min.js'))
+  .pipe(
+    uglify()
+      .on('error', function (err) {
+        gutil.log(gutil.color.red('[Error]'), err.toString());
+        this.emit('end');
+      })
+  )
+  .pipe(gulp.dest(config.outputDir + '/js'));
 });
 
-gulp.task('images', function() { 
-  return gulp.src(config.imagesPath + '/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest(config.outputDir + '/images'))
-});
-
-gulp.task('css', function() {
+gulp.task('css', function () {
   console.log(`building css at ${Date.now()}`);
   return gulp.src(config.stylesPath + '/main.scss')
     .pipe(bulkSass())
     .pipe(sass({
-        outputStyle: 'compressed',
-        includePaths: [
-          config.stylesPath,
-          './node_modules/bootstrap/scss',
-          './node_modules/font-awesome/scss'
-        ]
-      }).on('error', sass.logError))
+      outputStyle: 'compressed',
+      includePaths: [
+        config.stylesPath,
+        './node_modules/bootstrap/scss',
+        './node_modules/font-awesome/scss',
+      ]
+    }).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest(config.outputDir + '/css'))
-    .pipe(browserSync.reload({stream:true}));
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-
-gulp.task('jquery', function(){
-  return gulp.src('./node_modules/jquery/dist/jquery.min.js') 
-    .pipe(gulp.dest(config.outputDir + '/js')); 
+gulp.task('images', function () {
+  return gulp.src(config.imagesPath + '/*')
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopflipng: true,
+      jpegRecompress: false,
+      mozjpeg: true,
+      guetzli: false,
+      gifsicle: true,
+      svgo: false,
+      concurrent: 10,
+    }))
+    .pipe(gulp.dest(config.outputDir + '/images'))
 });
 
-gulp.task('popper', function(){
-  return gulp.src('./node_modules/popper.js/dist/umd/popper.min.js') 
-    .pipe(gulp.dest(config.outputDir + '/js')); 
-});
-
-gulp.task('bootstrap-js', function(){
-  return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js') 
-    .pipe(gulp.dest(config.outputDir + '/js')); 
-});
-
-gulp.task('js', function() {
-  return gulp.src(config.jsPath+'/*')
+gulp.task('js', function () {
+  return gulp.src(config.jsPath + '/*')
     .pipe(filter('**/*.js'))
-    .pipe(concat('main.js'))
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(
       uglify()
-        .on('error', function(err) {
+        .on('error', function (err) {
           gutil.log(gutil.color.red('[Error]'), err.toString());
           this.emit('end');
         })
     )
     .pipe(gulp.dest(config.outputDir + '/js'))
-    .pipe(browserSync.reload({stream:true, once: true}));
+    .pipe(browserSync.reload({ stream: true, once: true }));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('layouts', function () {
+  return gulp.src(config.layoutsPath + '/*.html')
+    .pipe(nunjucks.compile())
+    .pipe(gulp.dest(config.htmlPath))
+});
+
+/* LIVE SERVER */
+
+gulp.task('browser-sync', function () {
   browserSync.init(null, {
     server: {
       baseDir: "public"
@@ -99,15 +116,17 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('bs-reload', function() {
+gulp.task('bs-reload', function () {
   browserSync.reload();
 });
 
-gulp.task('build', tasks, function() {})
+/* COMMANDS */
 
-gulp.task('default', [...tasks, 'browser-sync'], function() {
-  gulp.watch([config.stylesPath + '**/*.scss', config.stylesPath + '**/*.sass', config.stylesPath + '**/*.css'], ['css']);
-  gulp.watch([config.jsPath + '**/*.js'], ['js']);
+gulp.task('build', tasks);
+
+gulp.task('default', [...tasks, 'browser-sync'], function () {
+  gulp.watch([config.stylesPath + '/**/*.scss'], ['css']);
+  gulp.watch([config.jsPath + '/**/*.js'], ['js']);
   gulp.watch([config.imagesPath + '/**/*'], ['images']);
-  gulp.watch([config.htmlPath + '**/*.html'], ['bs-reload']);
+  gulp.watch([config.layoutsPath + '/**/*.html'], ['layouts', 'bs-reload']);
 });
